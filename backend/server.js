8dabf3e1f9f41a55747ed99a70df562ee8cf2821
Jose L. Ulloa <jose.ulloa@isandex.com>
2022-11-26 17:31:50 -0300

    minor typo error

--- backend/server.js
+++ backend/server.js
@@ -28,12 +28,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(require('./routes/routes'));
 // app.use(require('./routes/users'));
 // for building (see https://dev.to/gregpetropoulos/render-deployment-free-tier-of-mern-app-52mk)
-console.log(path.resolve(__dirname, '..', 'frontend', 'build','index.html'))
 if (process.env.NODE_ENV === 'production') {
-
     //*Set static folder up in production
     app.use(express.static('../frontend/build'));
-
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build','index.html')));
   }
 
