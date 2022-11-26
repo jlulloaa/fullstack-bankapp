d548b9d4e4943e23060bf3d1d2d7dc79339a31af
Jose L. Ulloa <jose.ulloa@isandex.com>
2022-11-26 18:29:15 -0300

    still debugging

--- backend/authenticate/authenticate.js
+++ backend/authenticate/authenticate.js
@@ -7,7 +7,7 @@ const serviceAccount = {
   type: process.env.FIREBASE_ADMIN_TYPE,
   project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
   private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
   client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
   auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
